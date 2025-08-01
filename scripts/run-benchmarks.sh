b738670403d11946f10d76e915a0be4654dce7bb
Helmut Eller <eller.helmut@gmail.com>
2025-08-01 13:06:50 +0200

    Use shfmt to format shell scripts.

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -2,57 +2,77 @@
 
 set -eu
 
-declare -a EMACSES=(master igc)
-declare -a ELFILES=(scroll boehm-gc hash-equal hash-eql hash-eq compile print)
+declare -a EMACSES=(
+    igc
+    wgc
+    master
+    master-0.75
+    master-1.0
+    master-1.5
+    #igc headerless
+    #master
+    #no-mps no-mps-headerless
+)
+
+declare -a ELFILES=(
+    scroll
+    boehm-gc
+    hash-equal
+    hash-eql
+    hash-eq
+    compile
+    print
+    primes
+    #primesv
+    #sumloop
+    string
+    taxicab
+    pidigits
+)
+
 REPEAT=1
 CSVFILE=results.csv
-TRACETOOL=tracetool_stap
+#TRACETOOL=tracetool_stap
+TRACETOOL=tracetool_gnutime
 
 ulimit -d $((512 * 1024))
 
 source cleanup.sh
-source screen-pty.sh
 
-for ELFILE in "${ELFILES[@]}" ; do
+for ELFILE in "${ELFILES[@]}"; do
     "../emacses/${EMACSES[0]}" -Q -batch \
-    -eval "(byte-compile-file \"../elisp/$ELFILE.el\")" \
-    	|| exit 2
+        -eval "(byte-compile-file \"../elisp/$ELFILE.el\")" ||
+        exit 2
 done
 
-TIMEPID=''
-function kill_time_children {
-    if [ -n "$TIMEPID" ] ; then
-	for CHILD in $(ps --no-headers --ppid "$TIMEPID" -o '%p') ; do
-	    kill -SIGHUP $CHILD || :
-	done
-    fi
+function quit_screen {
+    local screenpid="$1"
+    screen -r "$screenpid" -X quit
 }
-
-CLEANUP_ACTIONS+=(kill_time_children)
 
 function tracetool_gnutime {
     local output=$1 emacs=$2 benchmark=$3
     shift 3
-    /usr/bin/time --output "$output" --append \
-		  --format "$emacs,$benchmark,%e,%U,%S,%M,0,0" \
-		  "$@"
+    exec screen -D -m /usr/bin/time --output "$output" --append \
+        --format "$emacs,$benchmark,%e,%U,%S,%M,0,0" \
+        "$@"
 }
 
 function tracetool_stap {
     local output=$1 emacs=$2 benchmark=$3
     local features=$(../emacses/$emacs \
-			 -Q -batch \
-			 -eval '(princ system-configuration-features)')
+        -Q -batch \
+        -eval '(princ system-configuration-features)')
     local stpfile
     case "$features" in
-	*\ MPS\ *)  stpfile=bench-mps.stp ;;
-	*) stpfile=bench-master.stp ;;
+        *\ MPS\ *) stpfile=bench-mps.stp ;;
+        *) stpfile=bench-master.stp ;;
     esac
     shift 3
     local tmpfile=$(mktemp)
     CLEANUP_ACTIONS+=("rm '$tmpfile'")
     stap -c "$*" -o "$tmpfile" $stpfile "$emacs" "$benchmark"
-    cat "$tmpfile" >> "$output"
+    cat "$tmpfile" >>"$output"
 }
 
 if [ -e "$CSVFILE" ]; then
@@ -63,18 +83,23 @@ printf "emacs,benchmark,\
 elapsed,user,sys,rss-max,\
 traced-time,ncollections\n" >"$CSVFILE"
 
-for EMACS in "${EMACSES[@]}" ; do
-    for ELFILE in "${ELFILES[@]}" ; do
-	for I in $(seq $REPEAT) ; do
-	    printf "$EMACS $ELFILE $I ..." >&2 
-	    $TRACETOOL "$CSVFILE" "$EMACS" "$ELFILE" \
-		"../emacses/$EMACS" -Q -nw -t "$SCREENPTY" \
-		-l "../elisp/${ELFILE}.elc" \
-		-f main 2>/dev/null &
-	    TIMEPID=$!
-	    wait $! || { echo "Emacs failed" >&2 ; exit 3 ; }
-	    printf "\r" >&2
-	    tail -n1 "$CSVFILE" >&2
-	done
+for EMACS in "${EMACSES[@]}"; do
+    for ELFILE in "${ELFILES[@]}"; do
+        for I in $(seq $REPEAT); do
+            printf "$EMACS $ELFILE $I ..." >&2
+            $TRACETOOL "$CSVFILE" "$EMACS" "$ELFILE" \
+                "../emacses/$EMACS" -Q -nw \
+                -l "../elisp/${ELFILE}.elc" \
+                -f main &
+            CLEANUP_ACTIONS+=("screen -r $! -X quit")
+            SCREENPID=$!
+            wait $! || {
+                printf "Emacs failed %d" "$?" >&2
+                exit 3
+            }
+            printf "\r" >&2
+            unset CLEANUP_ACTIONS[-1]
+            tail -n1 "$CSVFILE" >&2
+        done
     done
 done
