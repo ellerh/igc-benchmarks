cdd9b218ea6e5f6297582722251a150ef1a63e4b
Helmut Eller <eller.helmut@gmail.com>
2025-08-09 20:34:40 +0200

    Remove stp stuff

--- elisp/scroll.el
+++ elisp/scroll.el
@@ -22,13 +22,10 @@
   (find-file (expand-file-name "src/xdisp.c" source-directory))
   (let* ((buffer (current-buffer))
 	 (state (cons nil n))
-	 (timer (run-with-idle-timer 0.1 t #'scroll-timer-function state buffer
+	 (timer (run-with-idle-timer 0.1 t
+				     #'scroll-timer-function state buffer
 				     done)))
     (setcar state timer)))
-
-
-(defun main2 ()
-  (start 80 (lambda () (stp-end) (kill-emacs))))
 
 (defun main ()
   (start 80 #'kill-emacs))
